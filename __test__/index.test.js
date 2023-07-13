cb125260e099a0b698ceea71e657897c46ee8c9d
Manuel Salvador <manu.sacr@hotmail.com>
2023-07-13 04:03:50 -0300

    test added

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,5 @@
-const { sum } = require('./index');
-const { mul } = require('./mul');
+const { sum } = require('./index.js');
+const { mul } = require('./mul.js');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
