71850bba14239b07e7d8f15f0563350495ed996d
Manuel Salvador <manu.sacr@hotmail.com>
2023-07-13 04:03:12 -0300

    test added

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,9 +2,9 @@ const { sum } = require('./index');
 const { mul } = require('./mul');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(5);
+  expect(sum(1, 2)).toBe(3);
 });
 
 test('multiply 3 * 2 to equal 6', () => {
-  expect(sum(1, 2)).toBe(5);
+  expect(mul(1, 2)).toBe(5);
 });
